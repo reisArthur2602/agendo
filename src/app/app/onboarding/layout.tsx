36edd644285df8b7beed6dca8d054fd46084c8c9
arthur <contato.arthurreis222@gmail.com>
2025-06-18 09:56:59 -0300

    fix: proteger rotas de onboarding

--- src/app/app/onboarding/layout.tsx
+++ src/app/app/onboarding/layout.tsx
@@ -7,8 +7,8 @@ const OnboardingLayout = async ({ children }: PropsWithChildren) => {
   const session = await auth();
   if (!session?.user) redirect("/app");
 
-  // const userHasBusiness = await getBusinessWithUser(session?.user.id);
-  // if (userHasBusiness) redirect("/app/dashboard");
+  const userHasBusiness = await getBusinessWithUser(session?.user.id);
+  if (userHasBusiness) redirect("/app/dashboard");
 
   return (
     <div className="flex items-center justify-center bg-blue-50">
